3d3b487ef254fedbd0320cb25a2d784e5de7ac26
Joseph RM <joseph.ramirez@mawi.io>
2022-11-12 13:21:33 -0600

    OM-11: Set folder structure of clean arch and update compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { EnvironmentVariableModule } from './config/environment/environment.module'
+import { EnvironmentVariableModule } from './infrastructure/config/environment/environment.module'
+import { ControllersModule } from './infrastructure/controllers/controllers.module'
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module'
 
 @Module({
-  imports: [
-    EnvironmentVariableModule.register()
-  ],
+  imports: [EnvironmentVariableModule.register(), ControllersModule, ExceptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
